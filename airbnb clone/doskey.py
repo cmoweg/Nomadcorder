144e8ce34db870fead244739d1ab7a537b839c7c
cmoweg <cmoweg@gmail.com>
2020-04-11 16:32:24 +0900

    ~7

--- airbnb clone/doskey.py
+++ airbnb clone/doskey.py
@@ -2,7 +2,7 @@ import os
 
 os.system("doskey ga= git add .")
 os.system("doskey pmg=python manage.py migrate")
-os.system("doskey pmm=python manage.py makemigrations ")
+os.system("doskey pmm=python manage.py makemigrations")
 os.system("doskey pcs=python manage.py createsuperuser")
 os.system("doskey rs=python manage.py runserver")
 os.system("doskey pms=python manage.py shell ")
