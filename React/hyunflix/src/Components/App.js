070c7305b1d2e1bf627ca219fa7769d2887975d9
cmoweg <cmoweg@gmail.com>
2020-05-16 03:01:56 +0900

    ~2 setup

--- React/hyunflix/src/Components/App.js
+++ React/hyunflix/src/Components/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import Router from "Components/Router";
+import Header from "Components/Header";
 
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Router />
+      </>
+    );
+  }
 }
 
 export default App;
